2e7625733c3f49415a0338adb8864a7cf7e3b808
lichingchester <liching.chester@gmail.com>
2016-11-21 23:16:52 +0800

    update orderDB

--- ESDAssignment2/src/java/db/OrdersDB.java
+++ ESDAssignment2/src/java/db/OrdersDB.java
@@ -14,6 +14,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  *
@@ -110,45 +111,89 @@ public class OrdersDB {
     }
      
      
-//    public OrderBean queryByID(String ID) {
-//        Connection cnnct = null;
-//        PreparedStatement pStmnt = null;
-//
-//        OrderBean ib = null;
-//        try {
-//            //1.  get Connection
-//            cnnct = getConnection();
-//            String preQueryStatement = "SELECT * FROM  Orders WHERE orderID=?";
-//            //2.  get the prepare Statement
-//            pStmnt = cnnct.prepareStatement(preQueryStatement);
-//            //3. update the placehoder with id
-//            pStmnt.setString(1, ID);
-//            ResultSet rs = null;
-//            //4. execute the query and assign to the result 
-//            rs = pStmnt.executeQuery();
-//            if (rs.next()) {
-//                ib = new OrderBean();
-//                // set the record detail to the customer bean
-//                ib.setItemID(rs.getString(1));
-//                ib.setItemName(rs.getString(2));
-//                ib.setDesc(rs.getString(3));
-//                ib.setCategory(rs.getString(4));
-//                ib.setDesignerName(rs.getString(5));
-//                ib.setPrice(rs.getDouble(6));
-//                ib.setPath(rs.getString(7));
-//                ib.setSize(rs.getString(8));
-//            }
-//
-//            pStmnt.close();
-//            cnnct.close();
-//        } catch (SQLException ex) {
-//            while (ex != null) {
-//                ex.printStackTrace();
-//                ex = ex.getNextException();
-//            }
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
-//        return ib;
-//    }
+    public OrderBean queryByID(String ID) {
+        Connection cnnct = null;
+        PreparedStatement pStmnt = null;
+
+        OrderBean ob = null;
+        try {
+            //1.  get Connection
+            cnnct = getConnection();
+            String preQueryStatement = "SELECT * FROM  Orders WHERE orderID=?";
+            //2.  get the prepare Statement
+            pStmnt = cnnct.prepareStatement(preQueryStatement);
+            //3. update the placehoder with id
+            pStmnt.setString(1, ID);
+            ResultSet rs = null;
+            //4. execute the query and assign to the result 
+            rs = pStmnt.executeQuery();
+            if (rs.next()) {
+                ob = new OrderBean();
+                // set the record detail to the customer bean
+                ob.setOrderID(rs.getString(1));
+                ob.setItemID(rs.getString(2));
+                ob.setUserID(rs.getString(3));
+                ob.setSize(rs.getString(4));
+                ob.setDeliveryType(rs.getString(5));
+                ob.setDeliveryDate(rs.getDate(6));
+                ob.setDeliveryTime(rs.getTime(7));
+                ob.setDeliveryAddress(rs.getString(8));
+                ob.setStatus(rs.getString(9));
+            }
+
+            pStmnt.close();
+            cnnct.close();
+        } catch (SQLException ex) {
+            while (ex != null) {
+                ex.printStackTrace();
+                ex = ex.getNextException();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    
+    
+    public ArrayList queryOrders() {
+        Connection cnnct = null;
+        PreparedStatement pStmnt = null;
+
+        ArrayList list = new ArrayList();
+        OrderBean ob = null;
+        try {
+            cnnct = getConnection();
+            String preQueryStatement = "SELECT * FROM  Orders";
+            pStmnt = cnnct.prepareStatement(preQueryStatement);
+            //Statement s = cnnct.createStatement();
+            ResultSet rs = pStmnt.executeQuery();
+
+            while (rs.next()) {
+                ob = new OrderBean();
+                ob.setOrderID(rs.getString(1));
+                ob.setItemID(rs.getString(2));
+                ob.setUserID(rs.getString(3));
+                ob.setSize(rs.getString(4));
+                ob.setDeliveryType(rs.getString(5));
+                ob.setDeliveryDate(rs.getDate(6));
+                ob.setDeliveryTime(rs.getTime(7));
+                ob.setDeliveryAddress(rs.getString(8));
+                ob.setStatus(rs.getString(9));
+                list.add(ob);
+            }
+            
+            pStmnt.close();
+            cnnct.close();
+            
+            return list;
+        } catch (SQLException ex) {
+            while (ex != null) {
+                ex.printStackTrace();
+                ex = ex.getNextException();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 }
