140446be0157816e1fb0f90bd0f187704bdbd3c5
ken420a <ken420a@hotmail.com>
2016-11-23 04:28:29 +0800

    Admin del Order

---
+++ ESDAssignment2/src/java/servlet/deleteOrderServlet.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import bean.OrderBean;
+import db.OrdersDB;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ken42
+ */
+@WebServlet(name = "deleteOrderServlet", urlPatterns = {"/deleteOrderServlet"})
+public class deleteOrderServlet extends HttpServlet {
+
+    OrdersDB odb;
+    OrderBean ob;
+    
+    public void init(){
+        String username = this.getServletContext().getInitParameter("dbUser");
+        String password = this.getServletContext().getInitParameter("dbPassword");
+        String Url = this.getServletContext().getInitParameter("dbUrl"); 
+        odb = new OrdersDB(Url, username, password);
+    }
+    
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+           String id = request.getParameter("id");
+           
+           odb.delRecord(id);
+           
+           response.sendRedirect("managerHandle?action=orderps&message=deleteItem");
+           
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- ESDAssignment2/web/manager/deleteOrder.jsp
+++ ESDAssignment2/web/manager/deleteOrder.jsp
@@ -4,14 +4,63 @@
     Author     : ken42
 --%>
 
+<%@page import="bean.OrderBean"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Delete Order Page</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/admin.css">
+        <%
+            OrderBean ob=(OrderBean)request.getAttribute("ob");
+        %>
+        
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <section class="header">
+            <div id="bg">
+                <img src="/ESDAssignment2/img/LOGO.png">
+                <a href="#logout" class="navbar">Log out</a>
+            </div>         
+        </section>
+        
+        <div id="container">
+            <h1 style="font-size: 50px">Top Confidential Management Room</h1>
+            <br>
+            <ul>
+                 <li>
+                     <a href="managerHandle?action=item">Items Management</a>
+                 </li>
+                 <li>
+                     <a href="/managerHandle?action=report">Incompleted Report</a>
+                 </li>
+                 <li class="dropdown">
+                     <a href="#" class="dropbtn">Users Management</a>
+                     <div class="dropdown-content">
+                         <a href="/managerHandle?action=users">Confirmed Users</a>
+                         <a href="/managerHandle?action=ncusers">Unconfirmed Users</a>
+                     </div>
+                 </li>
+                 <li class="dropdown">
+                     <a href="#" class="dropbtn">Orders Management</a>
+                     <div class="dropdown-content">
+                         <a href="/managerHandle?action=orderps">Processing Orders</a>
+                         <a href="/managerHandle?action=orderhis">Orders History</a>
+                     </div>
+                 </li>
+             </ul>
+             <br>
+             
+             
+              <p class="deleteItem">Are you sure to delete the Order?</p>
+            <form action="deleteOrderServlet">
+                <input type="hidden" name="id" value="<%= ob.getOrderID() %>">
+                <input type="submit" value="Delete">
+                <a href="managerHandle?action=orderps"><input type="button" value="Cancel"></a>
+
+            </form>
+        </div>
     </body>
 </html>
