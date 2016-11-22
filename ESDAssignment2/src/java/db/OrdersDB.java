91f499008869a7ab918e610ba5cdc1d9bc54aa17
lichingchester <liching.chester@gmail.com>
2016-11-23 02:30:29 +0800

    fix

--- ESDAssignment2/src/java/db/OrdersDB.java
+++ ESDAssignment2/src/java/db/OrdersDB.java
@@ -87,7 +87,7 @@ public class OrdersDB {
                     + "size VARCHAR(10),"
                     + "deliveryType VARCHAR(20),"
                     + "deliveryDate DATE,"
-                    + "deliveryTime int,"
+                    + "deliveryTime Integer,"
                     + "deliveryAddress VARCHAR(100),"
                     + "status VARCHAR(20),"
                     + "quantity int,"
@@ -122,7 +122,7 @@ public class OrdersDB {
         boolean isSuccess = false;
         try {
             cnnct = getConnection();
-            String preQueryStatement = "INSERT  INTO  CUSTOMER  VALUES  (?,?,?,?,?,?,?,?,?,?,?)";
+            String preQueryStatement = "INSERT  INTO  Orders  VALUES (?,?,?,?,?,?,?,?,?,?,?)";
             pStmnt = cnnct.prepareStatement(preQueryStatement);
             pStmnt.setString(1, orderID);
             pStmnt.setString(2, groupID);
--- ESDAssignment2/src/java/testDB/TestAddRecord.java
+++ ESDAssignment2/src/java/testDB/TestAddRecord.java
@@ -19,37 +19,16 @@ public class TestAddRecord {
 
     public static void main(String[] arg) throws ParseException {
         OrderBean ob = new OrderBean();
-        OrdersDB db;
+        OrdersDB odb;
         String url = "jdbc:derby://localhost/database";
         String username = "app";
         String password = "app";
-        db = new OrdersDB (url, username, password);
+        odb = new OrdersDB (url, username, password);
         
 
         
-        String dDate="22/12/2016";
-        DateFormat dateformat = new SimpleDateFormat("mm/dd/yyyy");
-        Date d = ((Date)dateformat.parse(dDate));
-        
-        String size="XL";
-        
-        int qty=4;
-        
-        String OrderID="001";
-        String time="08:00";
-        
-        DateFormat df = new SimpleDateFormat("HH:mm");
-        
-//        Time t=java.sql.Time.valueOf("08:00");
-//        java.sql.Time timeValue = new java.sql.Time(formatter.parse(df.parse(time).toString()).getTime());
-        java.sql.Time myTime = java.sql.Time.valueOf(time);
-//        ob.setDeliveryTime(myTime);
-        ob.setDeliveryDate((java.sql.Date) d);
-        ob.setSize(size);
-        ob.setQuantity(qty);
-        ob.setOrderID(OrderID);
-                
-        db.editRecord(ob);
+        odb.createOrderTable();
+        odb.addRecord("002", "1", "1", "87654321", "M", "2", "2016-11-22", 5, "Flat2402", "deliverying", 1);
             
     }
 }
